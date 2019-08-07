823d9010becbbea2bd624fd94fee13ee391e2842
shivasrk12345 <shivasrk1234@ou.edu>
2019-08-06 21:04:31 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Main from './components/MainComponent';
 import React,{Component} from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+      </BrowserRouter>
     );
   }
 }
