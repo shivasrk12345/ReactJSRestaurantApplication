f4f154775f9f90ef8f3fcee4e4eca14ff7b22135
shivasrk12345 <shivasrk1234@ou.edu>
2019-08-12 18:27:57 -0500

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle} from 'reactstrap';
+
+function RenderCard({item}) {
+
+  return(
+      <Card>
+          <CardImg src={item.image} alt={item.name} />
+          <CardBody>
+          <CardTitle>{item.name}</CardTitle>
+          {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+          <CardText>{item.description}</CardText>
+          </CardBody>
+      </Card>
+  );
+
+}
 
 function Home(props) {
     return(
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
@@ -21,6 +25,9 @@ class Main extends Component {
     const HomePage = () => {
       return(
           <Home 
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -31,6 +38,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
